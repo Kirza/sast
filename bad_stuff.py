7b114f223b5af6621d1db70b96c4107da3ebdbd7
Kirza <Kirza@users.noreply.github.com>
2023-12-26 00:18:46 +0300

    Create bad_stuff.py

---
+++ bad_stuff.py
@@ -0,0 +1,42 @@
+from crypt import AES
+from Crypto.Cipher import DES
+print(are)
+print('are')
+from Crypto.Cipher import RC2
+from Crypto.Cipher import RC1
+from Crypto.Cipher import RC1, DES
+
+
+
+import socket
+
+print('recvfrom_into')
+recvfrom_into(1, 2, 3)
+s.recvfrom_into(1, 2, 3) # +
+s.recvfrom_into()
+
+
+
+from crypt import AES
+from Crypto.Cipher import DES
+print(are)
+print('are')
+from Crypto.Cipher import RC2
+from Crypto.Cipher import RC1
+from Crypto.Cipher import RC1, DES
+
+
+
+cfg.web('protocol', default='http', help='Default protocol.')
+cfg.web('protocol', default='https', help='Default protocol.')
+http = 1.1
+ver = 'httpversion'
+
+
+
+import are
+import json
+import ipboards
+
+print(are)
+print('are')
